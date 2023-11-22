2af9faff7fbd277c108ccdeb7817ef5760fbe205
cloudidespacetechnologies <150973773+cloudidespacetechnologies@users.noreply.github.com>
2023-11-22 10:11:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import ota  # Asegúrate de tener este módulo
 
 # Constantes para la versión del firmware y la URL de actualización
 FIRMWARE_VERSION = 1.0  # Debe ser un float
-UPDATE_URL = "https://nachobeta07.github.io/firmware_microPython.json"
+UPDATE_URL = "https://cloudidespacetechnologies.github.io/firmware_microPython.json"
 
 # Variables para el LED
 current_led_pin = 17  # GPIO para el LED actual
