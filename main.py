3e6e899caba97ae83ab610c9ee6dbf31bac5692e
cloudidespacetechnologies <150973773+cloudidespacetechnologies@users.noreply.github.com>
2023-11-23 23:03:19 -0500

    Update main.pyto version 1.0

--- main.py
+++ main.py
@@ -5,26 +5,39 @@ import _thread
 import ota  # Asegúrate de tener este módulo
 
 # Constantes para la versión del firmware y la URL de actualización
-FIRMWARE_VERSION = 1.1  # Debe ser un float
+FIRMWARE_VERSION = 1.0  # Debe ser un float
 UPDATE_URL = "https://cloudidespacetechnologies.github.io/firmware_microPython.json"
 
 # Aqui va las Variables para el Controlar los componentes
-current_led_pin = 18  # GPIO para el LED actual
-led = machine.Pin(current_led_pin, machine.Pin.OUT)
+# Pines para los componentes
+IR_SENSOR_PIN = 15  # Ejemplo, ajusta según tu conexión
+RELAY_PIN = 4       # Ejemplo, ajusta según tu conexión
+LED_PIN = 2         # Ejemplo, ajusta según tu conexión
 
-# Control para el bucle de parpadeo del LED
-stop_blinking = False
+# Configurar los pines
+ir_sensor = machine.Pin(IR_SENSOR_PIN, machine.Pin.IN)
+relay = machine.Pin(RELAY_PIN, machine.Pin.OUT)
+led = machine.Pin(LED_PIN, machine.Pin.OUT)
+
+
 
 def Devices_control():
     """
     Controla el funcionamiento del dispositivo.
     """
-    global stop_blinking
-    led.value(True)  # Enciende el LED inicialmente
-    while not stop_blinking:
-        led.value(not led.value())  # Cambia el estado del LED
-        time.sleep(0.5)  # Parpadeo cada 0.5 segundos
-    led.value(False)  # Asegura que el LED se apague al detener el parpadeo
+    while True:
+        # Lógica para el sensor infrarrojo
+        ir_value = ir_sensor.value()
+
+        # Lógica para controlar el relé y el LED
+        if ir_value == 1:  # Objeto detectado
+            relay.value(1)  # Encender relé
+            led.value(1)    # Encender LED
+        else:
+            relay.value(0)  # Apagar relé
+            led.value(0)    # Apagar LED
+
+        time.sleep(0.1)  # Ajusta según tus necesidades
 
 
 def main():
@@ -32,11 +45,10 @@ def main():
     Función principal que inicializa los hilos y procesos necesarios.
     """
     global stop_blinking
-    global current_led_pin
 
      # Configura el pin antiguo del LED a bajo para apagarlo si es necesario
    # Lista de pines del firmware anterior para apagarlos si es necesario
-    old_pins = [17]  # Asegúrate de actualizar esta lista según sea necesario
+    old_pins = [18]  # Asegúrate de actualizar esta lista según sea necesario
     for pin_number in old_pins:
       old_led = machine.Pin(pin_number, machine.Pin.OUT)
       old_led.value(False)  # Apaga el LED del pin antiguo
