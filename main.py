6639191d06be6b2d150e6fb5f06df55d93f36cb2
cloudidespacetechnologies <150973773+cloudidespacetechnologies@users.noreply.github.com>
2023-11-23 23:27:46 -0500

    Update main.pyto version 1.1

--- main.py
+++ main.py
@@ -5,19 +5,14 @@ import _thread
 import ota  # Asegúrate de tener este módulo
 
 # Constantes para la versión del firmware y la URL de actualización
-FIRMWARE_VERSION = 1.0  # Debe ser un float
+FIRMWARE_VERSION = 1.1  # Debe ser un float
 UPDATE_URL = "https://cloudidespacetechnologies.github.io/firmware_microPython.json"
 
 # Aqui va las Variables para el Controlar los componentes
-# Pines para los componentes
-IR_SENSOR_PIN = 15  # Ejemplo, ajusta según tu conexión
-RELAY_PIN = 4       # Ejemplo, ajusta según tu conexión
-LED_PIN = 2         # Ejemplo, ajusta según tu conexión
-
-# Configurar los pines
-ir_sensor = machine.Pin(IR_SENSOR_PIN, machine.Pin.IN)
+# Pin para el relé
+RELAY_PIN = 4  # Ejemplo, ajusta según tu conexión
+# Configurar el pin del relé
 relay = machine.Pin(RELAY_PIN, machine.Pin.OUT)
-led = machine.Pin(LED_PIN, machine.Pin.OUT)
 
 
 
@@ -25,19 +20,12 @@ def Devices_control():
     """
     Controla el funcionamiento del dispositivo.
     """
-    while True:
-        # Lógica para el sensor infrarrojo
-        ir_value = ir_sensor.value()
-
-        # Lógica para controlar el relé y el LED
-        if ir_value == 1:  # Objeto detectado
-            relay.value(1)  # Encender relé
-            led.value(1)    # Encender LED
-        else:
-            relay.value(0)  # Apagar relé
-            led.value(0)    # Apagar LED
-
-        time.sleep(0.1)  # Ajusta según tus necesidades
+     while True:
+        # Lógica para controlar el relé
+        relay.value(1)  # Encender relé
+        time.sleep(2)   # Mantener encendido durante 2 segundos
+        relay.value(0)  # Apagar relé
+        time.sleep(2)   # Esperar 2 segundos antes de encender nuevamente
 
 
 def main():
@@ -48,7 +36,7 @@ def main():
 
      # Configura el pin antiguo del LED a bajo para apagarlo si es necesario
    # Lista de pines del firmware anterior para apagarlos si es necesario
-    old_pins = [18]  # Asegúrate de actualizar esta lista según sea necesario
+    old_pins = [15,4,2]  # Asegúrate de actualizar esta lista según sea necesario
     for pin_number in old_pins:
       old_led = machine.Pin(pin_number, machine.Pin.OUT)
       old_led.value(False)  # Apaga el LED del pin antiguo
